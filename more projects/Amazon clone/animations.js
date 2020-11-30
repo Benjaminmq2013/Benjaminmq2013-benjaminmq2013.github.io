175ea843828efa4880209eed90310df8d2048ff6
Adriel Benjamin Martinez <Benjaminmq2013@gmail.com>
2020-11-30 17:25:40 -0600

    Minor bugs fixed

--- more projects/Amazon clone/animations.js
+++ more projects/Amazon clone/animations.js
@@ -9,11 +9,16 @@ function menu_controll (){
     menu_opacity_panel.classList.add("opacity-true")
     menu_opacity_panel.classList.remove("panel_display-none")
     menu_opacity_panel.classList.remove("opacity-false")
+
+
+
+    close_menu_btn.classList.remove("close_opacity-false")
+    close_menu_btn.classList.add("close_opacity-true")
 }
 
 // *******************  PANEL BACKGROUND OPACITY  **
 const menu_opacity_panel = document.querySelector(".menu_opacity-panel")
-
+const close_menu_btn = document.querySelector(".close_menu-btn")
 menu_opacity_panel.addEventListener("click", menu_controll_bye)
 
 
@@ -29,9 +34,11 @@ function menu_controll_bye(){
     panel_menu.classList.remove("right")
     panel_menu.classList.add("left")
     menu_opacity_panel.classList.remove("opacity-true")
-    
-
     menu_opacity_panel.classList.add("opacity-false")
+
+    close_menu_btn.classList.remove("close_opacity-true")
+    close_menu_btn.classList.add("close_opacity-false")
+    
 
     //Llamando a función desaparecer objetos después de opacarlos (1segundo)
     setTimeout(()=> {menu_controll_opacity()}, 500)
